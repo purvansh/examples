ea9b4f1ad2e8c11f86b863474b29763803f04b66
Purvansh Parmar <purvanshparmar15@gmail.com>
2021-05-25 13:13:52 +0530

    Update neural_style.py

--- fast_neural_style/neural_style/neural_style.py
+++ fast_neural_style/neural_style/neural_style.py
@@ -111,8 +111,7 @@ def train(args):
 
     # save model
     transformer.eval().cpu()
-    save_model_filename = "epoch_" + str(args.epochs) + "_" + str(time.ctime()).replace(' ', '_') + "_" + str(
-        args.content_weight) + "_" + str(args.style_weight) + ".model"
+    save_model_filename = "epoch.model"
     save_model_path = os.path.join(args.save_model_dir, save_model_filename)
     torch.save(transformer.state_dict(), save_model_path)
 
